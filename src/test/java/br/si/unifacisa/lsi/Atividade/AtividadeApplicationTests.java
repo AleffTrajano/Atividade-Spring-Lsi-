d36c1be54870ed95acdb67c159d91ec141aba5d1
aleff <aleff_diniz@hotmail.com>
2019-09-15 18:19:11 -0300

    Projeto iniciado

---
+++ src/test/java/br/si/unifacisa/lsi/Atividade/AtividadeApplicationTests.java
@@ -0,0 +1,16 @@
+package br.si.unifacisa.lsi.Atividade;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtividadeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
