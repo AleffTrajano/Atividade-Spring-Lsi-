674f7c75d4632f49baa45318cf21b8b616649bc3
aleff <aleff_diniz@hotmail.com>
2019-09-15 19:44:38 -0300

    Banco de dados H2 e criação

--- src/main/java/br/si/unifacisa/lsi/Atividade/domain/Categoria.java
+++ src/main/java/br/si/unifacisa/lsi/Atividade/domain/Categoria.java
@@ -2,18 +2,21 @@ package br.si.unifacisa.lsi.Atividade.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter@Setter 
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy =GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	public Categoria(Integer id, String nome) {
